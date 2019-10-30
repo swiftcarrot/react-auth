3f38e45050897c9d208ecc551f478c3dee676480
Wang Zuo <wzuoadjusted@gmail.com>
2019-10-30 16:55:12 +0800

    getCurrentUsr testing with act()

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,15 +1,67 @@
 import React from 'react';
-import { AuthProvider, AuthProtected } from '../';
-import renderer from 'react-test-renderer';
+import { AuthProvider, AuthProtected, AuthContext } from '../';
+import { create, act } from 'react-test-renderer';
 
 test('render', () => {
-  const App = () => {
-    return (
+  expect(
+    create(
       <AuthProvider>
         hello<AuthProtected>world</AuthProtected>
       </AuthProvider>
-    );
-  };
+    ).toJSON()
+  ).toMatchInlineSnapshot(`"hello"`);
+});
 
-  expect(renderer.create(<App />).toJSON()).toMatchInlineSnapshot(`"hello"`);
+test('loading', () => {
+  const Loading = () => 'loading';
+
+  expect(
+    create(
+      <AuthProvider>
+        hello
+        <AuthProtected renderLoading={Loading}>world</AuthProtected>
+      </AuthProvider>
+    ).toJSON()
+  ).toMatchInlineSnapshot(`
+    Array [
+      "hello",
+      "loading",
+    ]
+  `);
+});
+
+test('getCurrentUser', async () => {
+  let root;
+  let resolve;
+  function getCurrentUser() {
+    return new Promise(_resolve => {
+      resolve = _resolve;
+    });
+  }
+
+  act(() => {
+    root = create(
+      <AuthProvider getCurrentUser={getCurrentUser}>
+        hello
+        <AuthProtected>
+          <AuthContext.Consumer>
+            {({ currentUser }) => currentUser.name}
+          </AuthContext.Consumer>
+        </AuthProtected>
+      </AuthProvider>
+    );
+  });
+
+  expect(root.toJSON()).toMatchInlineSnapshot(`"hello"`);
+
+  await act(async () => {
+    resolve({ id: '1', name: 'test' });
+  });
+
+  expect(root.toJSON()).toMatchInlineSnapshot(`
+    Array [
+      "hello",
+      "test",
+    ]
+  `);
 });
